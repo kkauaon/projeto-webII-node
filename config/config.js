334c4d6a748a9f7045b2106bab00d9b66120815f
Kauã Sousa <kaua.sousa63@aluno.ifce.edu.br>
2025-10-14 20:28:44 -0300

    azure sql

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'administrador',
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: 'projetoweb2.database.windows.net',
+    dialect: 'mssql',
+  },
+  test: {
+    username: 'administrador',
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: 'projetoweb2.database.windows.net',
+    dialect: 'mssql',
+  },
+  production: {
+    username: 'administrador',
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: 'projetoweb2.database.windows.net',
+    dialect: 'mssql',
+  }
+};
