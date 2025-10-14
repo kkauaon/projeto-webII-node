43c6d999970d42317552b63956b6dacead3e1e08
Jot4g3 <gabriel.joao61@aluno.ifce.edu.br>
2025-10-14 16:26:24 -0300

    adições

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-app.listen(3001, () => {
-    console.log("Servidor rodando na porta 3001")
+const db = require('./models');
+
+const postRouter = require('./routes/Posts');
+app.use("/posts", postRouter);
+
+const commentRouter = require('./routes/Comments');
+app.use("/comments", commentRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () =>{
+    console.log("Servidor rodando na porta 3001");
+    })
 })
+
